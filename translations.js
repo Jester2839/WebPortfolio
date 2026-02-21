ec93643d2a55af9a113d9e28e3419fae23e984e7
Jakub Nový <jakub.jester@gmail.com>
2026-02-21 15:29:20 +0100

    .

--- translations.js
+++ translations.js
@@ -9,12 +9,13 @@ const translations = {
         // "btn_text": "Cz",
         
         // Hero sekce
-        "hero_status": "Open to work",
+        "hero_status": "Available for collaboration",
         "hero_download": "Download",
         "hero_im": "I'm",
         "role_1": "Photographer",
-        "role_2": "Web Designer",
-        "role_3": "Coder",
+        "role_2": "Web Developer",
+        "role_3": "C# Programmer",
+        "role_4": "IT Student",
         
         // Summary karty
         "h2_summary": "What am I...",
@@ -68,17 +69,18 @@ const translations = {
         "hero_download": "Stáhnout",
         "hero_im": "Jsem",
         "role_1": "Fotograf",
-        "role_2": "Web Designér",
-        "role_3": "Programátor",
+        "role_2": "Web Developer",
+        "role_3": "C# Programátor",
+        "role_4": "IT Student",
         
         // Summary karty
         "h2_summary": "Co jsem zač...",
         "card1_title": "Začátky a studie",
-        "card1_text": "Provizorni text aaaaaa aaaaaaa aaaaaaaaaaa aaaa aa aaaaaa aaaaaa aaaaaaaa aaaa aaaa",
+        "card1_text": "Jsem prostý kluk z předměstí Pardubic, co se na základce zamiloval do kódu a na střední do focení a pak si neúprosně šel za svým snem. První stupeň jsem se prorval, každý týden jsem domu nosil poznámky a rodičům ze školy furt volali co jsem provedl. Pak jsem se začal věnovat bojovým sportům a uměním a konečně jsem začal sekat dobrotu. Pak jsem přestoupil na matematickou školu a tam jsem objevil co ve mě je. A právě tam mě začalo bavit programovat a hrabat se v kódu. Proto když jsem se v 8. třídě dozvěděl o Deltě, bylo jasný kam půjdu dál. Dělal jsem všechno proto, abych se tam dostal.... a vyplatilo se. Když jsem tam konečně nastoupil a začala “má léta na deltě“. Až tady jsem objevil svůj pravý potenciál a začal se plně věnovat tomu co mě baví a naplňuje.",
         "card2_title": "Cestování a fotografie",
-        "card2_text": "Provizorni text aaaaaa aaaaaaa aaaaaaaaaaa aaaa aa aaaaaa aaaaaa aaaaaaaa aaaa aaaa",
+        "card2_text": "Už od malička s námi rodiče a prarodiče cestovali co to šlo a postupem let se to už jen stupňovalo. Jelikož jsme lezecká rodina tak to vždycky bylo někam na hory do skal.… a to mi tak nějak zůstalo. Nejsme taková ta rodina, která jednou ročně jede na týden ležet na pláže, my radši jedem 4x ročně na pár dní lézt, skialpovat nebo jen chodit po horách, protože to je proste lepší a ty výhledy.... to se proste nedá srovnávat s nějakým resortem u moře. No proste do hor jsem začal utíkal co nejdřív to šlo, už v 16 jsem si spontánně šel na 3 dny projít do hor, a ne že bych spal v hotelech a jedl v restauracích... to vůbec na zádech 10 kg a spát na divoko, jinak to proste není ono. A s tím se mi krásně propojil můj nový koníček “focení“. A jak jsem se k němu vůbec dostal? no…. Na konci prváku jsme jeli na týden se učit fotit do Litomyšle. A tam jsem našel přesně TO, co my chybělo, nějaká kreativní činnost, a to přesně pro mě je focení. Já jsem věděl že celý život jsem měl cit pro detail a aspoň nějakého toho kreativního ducha, ale až při focení jsem to fakt pocítil. Když mi ty fotky začaly lidi chválit věděl jsem, že tohle mě bude bavit ještě dlouho. Začínal jsem s vyřazeným, 20 let starým foťákem od babičky. Nebyl to žádný hit, ale nějaký hezký fotky z toho vylezli a byl to perfektní stroj na začátek a dovolím si říct že to i po tolika letech je pořád konkurence schopné. Co si budeme vše, co tu na webu vidíte je přece jen z něj. Neříkám, že jsem v tom dobrý, ale myslím a věřím že v tom mám nějakou aspoň amatérskou budoucnost.",
         "card3_title": "Kód a web designe",
-        "card3_text": "Provizorni text aaaaaa aaaaaaa aaaaaaaaaaa aaaa aa aaaaaa aaaaaa aaaaaaaa aaaa aaaa",
+        "card3_text": "Jak jsem říkal ke kódu jsem se dostal už na základce, ne že bych v tom byl dobrý ale docela mě to bavilo a neměl jsem jiné nápady kam se zaměřit. Tak jsem se dal na deltu a tam to ze mě vytáhly, tenhle talent nebo co. K programování mi vlastně dost pomáhá fakt, že mám smysl pro detail a jsem sakra velkej perfekcionista. Takže když se do něčeho dám tak se do toho dám naplno a ať to stoji co to stojí, tak to proste dostanu do svého pomyslného perfektního konce. Když jsem pak v druhém ročníku se začal učit web designe a vytvářet webovky, konečně jsem mohl spojit logiku s kreativitou a udělat z toho jedno. Takže spojením všech mích švarů i nešvarů jsem vždy udělal nějaký přepal, všichni kolem mě udělali nutné minimum, ale já…. já to měl vždycky vymyšlené a dotáhnuté do mého megalomanského výmyslu. Web je věc, která mě jako jedna z mála začal naplňovat a co si budeme není to úplně špatný přivýdělek ke studiu, jsem sice na začátku ale věžím že postupem času to dostanu na dostatečně velkou úroveň abych že se budu moct srovnávat se špičkou v oboru.",
 
         //skills sekce
         "h2_skills": "Co umím...",
